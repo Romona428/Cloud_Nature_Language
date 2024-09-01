626529fe77741d78194f01caf8754222a4ce6bc0
Romona <a.cychu@gmail.com>
2024-09-01 08:46:41 +0800

    upload_to_bigQuery

--- entity_to_tags.py
+++ entity_to_tags.py
@@ -4,47 +4,50 @@ import pandas as pd
 from google_cnl_api import google_cnl_api
 from google.cloud import translate_v2 as translate
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:\\Users\\T14 Gen 3\\Downloads\\my-project-tir102-753ec9549127.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r"C:\Users\user\Documents\Keys\my-project-tir102-753ec9549127.json"
 
 df = pd.read_json('apod_data_1995-06-16_1997-12-31.json')   
 
-# 儲存每次迴圈中創建的 DataFrame 的列表
-dataframes = []
+def entity_to_tags(df):
 
-# for i in range(1, len(df)):
-for i in range(0, 10):
-    content_string = df.iloc[i]['explanation']
-    fig_date = df.iloc[i]['date']
-    print("-----",content_string,"-----")
-    tags_list = google_cnl_api(content_string)
+    # 儲存每次迴圈中創建的 DataFrame 的列表
+    dataframes = []
 
-    # 初始化翻譯客戶端
-    translate_client = translate.Client()
-    # 翻譯結果列表
-    translated_array = []
-    # 將英文翻譯成中文
-    for text in tags_list:
-        result = translate_client.translate(text, target_language='zh-TW')
-        print(result)
-        translated_array.append(result['translatedText'])
+    # for i in range(1, len(df)):
+    for i in range(0, 10):
+        content_string = df.iloc[i]['explanation']
+        fig_date = df.iloc[i]['date']
+        print("-----",content_string,"-----")
+        tags_list = google_cnl_api(content_string)
 
-
-    print("---------------",i,"------------------")
-    print(len(tags_list))
-    print(len(translated_array))
+        # 初始化翻譯客戶端
+        translate_client = translate.Client()
+        # 翻譯結果列表
+        translated_array = []
+        # 將英文翻譯成中文
+        for text in tags_list:
+            result = translate_client.translate(text, target_language='zh-TW')
+            print(result)
+            translated_array.append(result['translatedText'])
 
 
-    df_tags=pd.DataFrame({
-        'tags_en' : tags_list,
-        'tags_zhTW' : translated_array 
-    })
+        print("---------------",i,"------------------")
+        print(len(tags_list))
+        print(len(translated_array))
 
-    # 在 DataFrame 最前面插入一個 date 欄位，值為 fig_date
-    df_tags.insert(0, 'date', fig_date)
-    # print(df_tags)
-    dataframes.append(df_tags)
 
-# 合併所有 DataFrame
-df_combined = pd.concat(dataframes, ignore_index=True)
+        df_tags=pd.DataFrame({
+            'tags_en' : tags_list,
+            'tags_zhTW' : translated_array 
+        })
 
-print(df_combined)
+        # 在 DataFrame 最前面插入一個 date 欄位，值為 fig_date
+        df_tags.insert(0, 'date', fig_date)
+        # print(df_tags)
+        dataframes.append(df_tags)
+
+    # 合併所有 DataFrame
+    df_combined = pd.concat(dataframes, ignore_index=True)
+
+    print(df_combined)
+    return df_combined
---
+++ upload_to_bigQuery.py
@@ -0,0 +1,41 @@
+#pip install pandas-gbq google-cloud-bigquery
+
+import pandas as pd
+from google.cloud import bigquery
+from google.oauth2 import service_account
+from pandas_gbq import to_gbq 
+from entity_to_tags import entity_to_tags
+
+
+df = pd.read_json('apod_data_1995-06-16_1997-12-31.json')  
+df_combined = entity_to_tags(df)
+
+#認證
+key_file_path = r"C:\Users\user\Documents\Keys\bigquery-romona.json"
+credentials = service_account.Credentials.from_service_account_file(key_file_path)
+
+# 建立 BigQuery 客戶端
+client = bigquery.Client(credentials=credentials, project='my-project-tir102')
+
+# 設定目標資料集和表格名稱
+table_id = 'my-project-tir102.demo.df_combined'
+
+# 手動定義模式
+schema = [
+    bigquery.SchemaField("date", "DATE"),
+    bigquery.SchemaField("tags_en", "STRING"),
+    bigquery.SchemaField("tags_zhTW", "STRING"),
+]
+
+# 設定 job 的配置
+job_config = bigquery.LoadJobConfig(
+    schema=schema,  
+    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE  # WRITE_APPEND
+)
+
+# 將 DataFrame 寫入 BigQuery
+job = client.load_table_from_dataframe(df_combined, table_id, job_config=job_config)
+job.result()
+
+print(f"已將 {df_combined.shape[0]} 列數據成功寫入 BigQuery")
+
